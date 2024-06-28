6ec1c63ffc61a01a470695fc7a64e3e42c45b501
TheRaj200 <102717203+TheRaj200@users.noreply.github.com>
2024-06-28 18:21:15 +0530

    new featues

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,23 @@ import './index.css';
 function App() {
   const initialData = [];
   const [visible, setVisible] = useState(true);
+  const [on, seton] = useState(false);
   const [text, setText] = useState('');
   const [data, setdata] = useState(initialData);
   const [num1, setNum1] = useState("");
   const [num2, setNum2] = useState("");
   const [sum, setSum] = useState();
   const[counter, setCounter] =useState(0);
-  const Data = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry', 'Fig', 'Grape'];
+  const Data = ['Apple',
+    'Banana',
+    'Orange',
+    'Strawberry',
+    'Grapes',
+    'Watermelon',
+    'Kiwi',
+    'Elderberry',
+     'Fig', 
+     'Grape'];
   const [searchText, setSearchText] = useState('');
 
   // Function to filter records based on search text
@@ -30,27 +40,31 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App ">
 
-      <section>
+      <section className=' relative bg-zinc-800 mb-5 p-10 border-2 rounded-md border-zinc-700'>
         <h2 >Sum Two Numbers</h2>
         <div id='inputs'>
-        <input className='gap'
+        <input className='gap py-[5px] rounded-md pl-5'
           type="number"
           value={num1}
           onChange={(e) => setNum1(e.target.value)}
           />
         <input
           type="number"
+           className='gap py-[5px] rounded-md pl-5'
           value={num2}
           onChange={(e) => setNum2(e.target.value)}
           /></div>
-        <button id='btn' onClick={handleSum} disabled={num1 === '' || num2 === ''}>
+        <button id='btn' onClick={handleSum} disabled={on}>
           Sum
+        </button>
+        <button className='absolute left-[28vh]' id='btn' onClick={() => seton(!on)}>
+        {on? 'enabled' : 'disable'}
         </button>
         <p id='btn' >Result : {sum}</p>
       </section>
-      <section>
+      <section className='bg-zinc-800 mb-5  p-10 border-2 rounded-md border-zinc-700'>
         <h2>Records</h2>
         {visible && (
           <ol>
@@ -59,30 +73,31 @@ function App() {
             ))}
           </ol>
         )}
-        <div className='bg-red-800'>
+        <div >
         <button className='gap' onClick={() => setVisible(!visible)}>
           {visible ? 'Hide' : 'Show'}
         </button>
         <button onClick={removeChild}>Remove Record</button>
        </div>
       </section>
-      <section>
-        <h2>Counter</h2>
+      <section className='bg-zinc-800 mb-5  p-10 border-2 rounded-md border-zinc-700'>
+        <h2 className='pb-5'>Counter</h2>
         <div id='flex'>
         <button  onClick={() => setCounter(pre=>  pre <= 0 ?  0 : pre - 1)}>
-              Decrement
+            -
         </button>
-        <p  >{counter}</p>
+        <p className='pt-2'  >{counter}</p>
         <button  onClick={() => setCounter(counter + 1)}>
-      Increment
+      +
         </button>
         </div>
 
       </section>
-      <section>
+      <section className='bg-zinc-800 mb-5  p-10 border-2 rounded-md border-zinc-700'>
         <h2>Search Filter</h2>
         <input
           type="text"
+           className='gap py-[5px] rounded-md pl-5'
           placeholder="Search..."
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
@@ -93,9 +108,10 @@ function App() {
           ))}
         </ol>
       </section>
-        <section>
+        <section className='bg-zinc-800 mb-5  p-10 border-2 rounded-md border-zinc-700'>
           <h2>Two-Way Data Binding</h2>
           <input
+           className='gap py-[5px] rounded-md pl-5'
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
